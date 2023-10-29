b4cc0c0c712cf150a75c2b08c1292eea55af9a99
rakibsarowar <rakibsarowar@gmail.com>
2023-10-29 22:23:30 +0600

    project done

--- index.js
+++ index.js
@@ -5,4 +5,6 @@ function setTime() {
     target.innerHTML = `${d.getHours()} : ${d.getMinutes()} : ${d.getSeconds()}`
 }
 
+
+// Just have to use function name, not call operator. 
 setInterval(setTime,1000)
